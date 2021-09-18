4579de51e8047b77b5f02e8b0e9d3457cc5ad435
kelechi <oparaiskelechi@gmail.com>
2021-09-18 23:55:21 +0100

    handle the location permission

--- app/src/main/java/com/asterisk/runtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/asterisk/runtracker/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.asterisk.runtracker.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -8,10 +10,14 @@ import androidx.navigation.fragment.findNavController
 import com.asterisk.runtracker.R
 import com.asterisk.runtracker.databinding.FragmentRunBinding
 import com.asterisk.runtracker.ui.viewmodels.MainViewModel
+import com.asterisk.runtracker.utils.Constants.REQUEST_LOCATION_PERMISSION_CODE
+import com.asterisk.runtracker.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment: Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel by viewModels<MainViewModel>()
 
@@ -20,6 +26,8 @@ class RunFragment: Fragment(R.layout.fragment_run) {
 
         val binding = FragmentRunBinding.bind(view)
 
+        requestPermissions()
+
         binding.apply {
 
             fab.setOnClickListener {
@@ -27,5 +35,50 @@ class RunFragment: Fragment(R.layout.fragment_run) {
                 findNavController().navigate(action)
             }
         }
+    }
+
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermission(requireContext())) {
+            return
+        } else {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.miss_permission),
+                    REQUEST_LOCATION_PERMISSION_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } else {
+                EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.miss_permission),
+                    REQUEST_LOCATION_PERMISSION_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            }
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
--- app/src/main/java/com/asterisk/runtracker/utils/Constants.kt
+++ app/src/main/java/com/asterisk/runtracker/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.asterisk.runtracker.utils
 
 object Constants {
     const val DATABASE_NAME = "run_database"
+    const val REQUEST_LOCATION_PERMISSION_CODE = 1
 }
